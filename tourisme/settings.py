c226d940c617b2d1ad8c63481914d8353713011d
thierry <thierry.devp@gmail.com>
2025-04-01 10:27:44 +0000

    last

--- tourisme/settings.py
+++ tourisme/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)2x-%c2pcg1l@k(*b-alk+1%otv6e+4@tk^_cj9o_ssm54vr%&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['welcometosafari.com','www.welcometosafari.com']
 #ALLOWED_HOSTS = ['*']
